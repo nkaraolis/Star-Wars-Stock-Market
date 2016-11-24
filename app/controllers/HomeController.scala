b64401ff4562feda2de3824ecdfaae3acd921b6e
Nick Karaolis <nickkaraolis@yahoo.com>
2016-11-24 16:36:42 +0000

    Added template files for homepage, registration and trader details page. Updated controllers to use injection and added messages file

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,19 @@
 package controllers
 
-import javax.inject._
-import play.api._
-import play.api.mvc._
+import javax.inject.{Inject, Singleton}
+
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+import play.api.mvc.{Action, Controller}
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * Created by Nick Karaolis on 24/11/16.
+  */
+
 @Singleton
 class HomeController @Inject() extends Controller {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def home = Action {
+    Ok(views.html.homepage())
   }
-
 }
--- app/controllers/TraderAccountController.scala
+++ app/controllers/TraderAccountController.scala
@@ -1,27 +1,36 @@
 package controllers
 
+import javax.inject.{Inject, Singleton}
+
 import models.TraderDetailsForm
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
 import services.TraderAccountService
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 
 /**
   * Created by Nick Karaolis on 21/11/16.
   */
 
-trait TraderAccountController extends Controller {
-  val traderAccountService: TraderAccountService
+@Singleton
+class TraderAccountController @Inject() extends Controller {
+  val traderAccountService = new TraderAccountService
 
-  def displayDetails: Unit = {
-    Ok(views.html.traderDetails(traderAccountService.retrieveTraderDetails))
-
+  def displayDetails = Action {
+    Ok(views.html.user.information.traderDetails(traderAccountService.retrieveTraderDetails))
   }
 
   def updateDetails: Unit = {
     traderAccountService.updateTraderDetails(traderAccountService.retrieveTraderDetails)
     TraderDetailsForm.traderDetailsForm.hasErrors
   }
-}
 
-object TraderAccountController extends TraderAccountController {
-  val traderAccountService = new TraderAccountService
+  def registration = Action {
+    implicit request =>
+      val form = if (request2flash.get("error").isDefined)
+        TraderDetailsForm.traderDetailsForm.bind(request2flash.data)
+      else
+        TraderDetailsForm.traderDetailsForm
+      Ok(views.html.user.information.registration(form))
+  }
 }
