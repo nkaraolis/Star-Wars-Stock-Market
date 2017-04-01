ce363bd5d1e05a28e11ce8b18d4ec99bbbaaf44d
Nick Karaolis <nickkaraolis@yahoo.com>
2017-04-01 11:14:19 +0100

    Updated sbt plugins, added auth header check to trader account controller and added request to registration page

--- app/controllers/TraderAccountController.scala
+++ app/controllers/TraderAccountController.scala
@@ -3,22 +3,26 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import models.{LoginForm, TraderDetailsForm}
-import play.api.i18n.{I18nSupport, Messages, MessagesApi}
-import play.api.mvc.{Action, AnyContent, Controller, Flash}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, Controller}
 import services.TraderAccountService
 
 import scala.concurrent.Future
+import scala.util.Random
 
 /**
   * Created by Nick Karaolis on 21/11/16.
   */
 
 @Singleton
-class TraderAccountController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
-  val traderAccountService = new TraderAccountService
+class TraderAccountController @Inject()(val messagesApi: MessagesApi,
+                                        traderAccountService: TraderAccountService) extends Controller with I18nSupport {
 
   def displayTraderDetails = Action { implicit request =>
-    Ok(views.html.user.information.traderDetails(traderAccountService.retrieveTraderDetails))
+    request.headers.get("Authorization") match {
+      case None => Redirect(routes.TraderAccountController.displayLogin())
+      case Some(_) => Ok(views.html.user.information.traderDetails(traderAccountService.retrieveTraderDetails))
+    }
   }
 
   def submitUpdateTraderDetails = TODO
@@ -33,10 +37,9 @@ class TraderAccountController @Inject()(val messagesApi: MessagesApi) extends Co
     Ok(views.html.user.information.login(LoginForm.loginForm))
   }
 
-  def submitLogin:Action[AnyContent] = Action.async { implicit request =>
+  def submitLogin: Action[AnyContent] = Action.async { implicit request =>
     LoginForm.loginForm.bindFromRequest().fold(
       formWithErrors => Future.successful(BadRequest(views.html.user.information.login(formWithErrors))),
-      validFormData => Future.successful(Redirect(routes.HomeController.home()))
-    )
+      validFormData => Future.successful(Redirect(routes.HomeController.home()).withHeaders(AUTHORIZATION -> Random.nextString(50))))
   }
 }
