c62d120761cebdf6753be994b90bb3446683b08c
Nick Karaolis <nickkaraolis@yahoo.com>
2017-04-07 16:12:04 +0100

    Renamed resources and currencies to include spaces in names Added css for grid layout of resources and currencies Removed unrequired sidebar link and updated sidebar links

--- app/models/Resource.scala
+++ app/models/Resource.scala
@@ -17,7 +17,7 @@ class Exonium extends Resource(resourceID = 4, name ="Exonium")
 
 class Ionite extends Resource(resourceID = 5, name ="Ionite")
 
-class LightsaberCrystal extends Resource(resourceID = 6, name ="LightsaberCrystal")
+class LightsaberCrystal extends Resource(resourceID = 6, name ="Lightsaber Crystal")
 
 class Ore extends Resource(resourceID = 7, name ="Ore")
 
@@ -27,10 +27,10 @@ class Stygium extends Resource(resourceID = 9, name ="Stygium")
 
 class Thorilide extends Resource(resourceID = 10, name ="Thorilide")
 
-class RepublicCredit extends Resource(resourceID = 11, name ="RepublicCredit", resourceType = "currencies")
+class RepublicCredit extends Resource(resourceID = 11, name ="Republic Credit", resourceType = "currencies")
 
 class Wupiupi extends Resource(resourceID = 12, name ="Wupiupi", resourceType = "currencies")
 
 class Druggat extends Resource(resourceID = 13, name ="Druggat", resourceType = "currencies")
 
-class NovaCrystal extends Resource(resourceID = 14, name ="NovaCrystal", resourceType = "currencies")
+class NovaCrystal extends Resource(resourceID = 14, name ="Nova Crystal", resourceType = "currencies")
